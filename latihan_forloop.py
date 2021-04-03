9f16d3850b00e82e5291efb2847a9963f5f427ad
Rafika Sari <rafikasari2909@gmail.com>
2021-04-03 07:07:37 +0700

    latihan membuat program data customer belum sesuai

---
+++ latihan_forloop.py
@@ -0,0 +1,15 @@
+# Create program that receive n customer name and age data,
+# then print all of the customer data
+# where n is an integer
+
+customer = str(input("Nama customer: "))
+age = float(input("Berapa umur anda: "))
+print("Hello",customer, "umur anda adalah",age, "tahun")
+
+#for x in customer and y in age: 
+ #   print("Customer= "+ x, "and age= "+ age)
+#for y in age:
+ #   print(y)
+
+# Catatan :
+# Program yg sudah dibuat sepertinya belum sesuai dengan perintah yang diinginkan
