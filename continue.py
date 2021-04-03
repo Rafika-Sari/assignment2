89c8b1a8bc5b4887a511ffeb559383717bbb0978
Rafika Sari <rafikasari2909@gmail.com>
2021-04-04 06:04:22 +0700

    materi while, continue dan nested loop belum paham

---
+++ continue.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    if i == 3:
+        continue
+    print(n)
+
+# belum paham maksudnya, dan belum paham bedanya dgn while
---
+++ while.py
@@ -0,0 +1,7 @@
+while True:
+    i = 1
+    while i < 6:
+        print(i)
+        i += 1
+
+# belum paham maksudnya
